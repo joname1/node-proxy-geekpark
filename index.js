0405836243d79cf705e3bcaa742dc6f5a8ef062c
joname1 <274158741@qq.com>
2018-03-30 10:57:45 +0800

    2nd commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var request = require('superagent');
-var charset = require('superagent-charset');
+//var charset = require('superagent-charset');
 
 var app = express();
 var HOST = 'http://main_test.geekpark.net/api/v1';
@@ -11,13 +11,13 @@ app.all('*', function (req, res, next) {
   res.set('Access-Control-Allow-Origin', '*');
   res.set('Access-Control-Allow-Methods', 'GET');
   res.set('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type');
-  res.set('Content-Type', 'text/html;charset=gb2312');
+  res.set('Content-Type', 'text/html;charset=utf-8');
   if ('OPTIONS' == req.method) return res.send(200);
   next();
 });
 
 app.get('/', function (req, res) {
-  var sreq = request.get(HOST + req.originalUrl).charset()
+  var sreq = request.get(HOST + req.originalUrl)
   sreq.pipe(res);
   sreq.on('end', function (error, res) {
     console.log('end');
@@ -25,7 +25,7 @@ app.get('/', function (req, res) {
 })
 
 app.get('/posts/:id', function (req, res) {
-  var sreq = request.get(HOST + req.originalUrl).charset()
+  var sreq = request.get(HOST + req.originalUrl)
   sreq.pipe(res);
   sreq.on('end', function (error, res) {
     console.log('end');
