8c858979d76a59bc741a0b4872df9836c66928f5
joname1 <274158741@qq.com>
2018-03-30 14:13:11 +0800

    update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const request = require('superagent');
 
 const app = express();
-const HostAPI = 'http://main_test.geekpark.net/api/v1';
+const HostAPI = 'http://main_test.geekpark.net/api';
 app.set('port', (process.env.PORT || 5000));
 
 app.all('*', ((req, res, next) => {
@@ -15,23 +15,36 @@ app.all('*', ((req, res, next) => {
 }));
 
 app.get('/', ((req, res) => {
+  res.end('<h1>Forbidden</h1>');
+}));
+
+app.get('/v1', ((req, res) => {
   let sreq = request.get(HostAPI + req.originalUrl)
   sreq.pipe(res);
   res.setHeader('Content-Type', 'text/html; charset=utf-8');
-  sreq.on('end', function (error, res) {
-    console.log('home: ok');
-  });
+  sreq.on('end', ((error, res) => {
+    console.log('v1: ok');
+  }));
+}));
+
+app.get('/v2', ((req, res) => {
+  let sreq = request.get(HostAPI + req.originalUrl)
+  sreq.pipe(res);
+  res.setHeader('Content-Type', 'text/html; charset=utf-8');
+  sreq.on('end', ((error, res) => {
+    console.log('v2: ok');
+  }));
 }));
 
 app.get('/posts/:id', ((req, res) => {
   let sreq = request.get(HostAPI + req.originalUrl)
   sreq.pipe(res);
   res.setHeader('Content-Type', 'text/html; charset=utf-8');
-  sreq.on('end', function (error, res) {
+  sreq.on('end', ((error, res) => {
     console.log('posts: ok');
-  });
+  }));
 }));
 
 app.listen(app.get('port'), (() => {
-  console.log('Node app is running on localhost:', app.get('port'));
+  console.log('Node app is running on http://localhost:'+ app.get('port'));
 }));
